86d3e33237cafe036c30285c66b3f6c97fd3c034
Simon Dybdahl Andersen <sdybdahl8@gmail.com>
2023-10-05 00:40:52 +0200

    Update Source.cpp

--- Week_5_6_RecursionAndArraySorting/Exercise_2/Source.cpp
+++ Week_5_6_RecursionAndArraySorting/Exercise_2/Source.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-
-void printStars(int n)
-{
-    if (n == 0)
-    {
-        return;
-    }
-    std::cout << "*";
-    printStars(n - 1);
-}
+using namespace std;
 
 void triangle(int m, int n)
 {
     if (m > n)
     {
-        return;
+        return; // Base case: stop recursion when m is greater than n
     }
-    printStars(m);
-    std::cout << std::endl;
-    triangle(m + 1, n);
-    printStars(m);
-    std::cout << std::endl;
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
+
+    triangle(m + 1, n); // Recursively call triangle with m+1
+
+    for (int i = 0; i < m; i++)
+    {
+        cout << "*";
+    }
+    cout << endl;
 }
 
 int main()
