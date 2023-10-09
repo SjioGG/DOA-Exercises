12c3d8c7452d720c8f329e51eb9225b60f5e4c0d
Simon Dybdahl Andersen <sdybdahl8@gmail.com>
2023-10-09 23:35:18 +0200

    Final

--- Week_5_6_RecursionAndArraySorting/Exercise_7/stlsort.cpp
+++ Week_5_6_RecursionAndArraySorting/Exercise_7/stlsort.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <time.h>
 #include <chrono>
-#include <sort.h>
+#include "sort.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -31,7 +31,7 @@ void permute(vector<AnyType> &a)
 
 int main()
 {
-    const int NUM_ITEMS = 5000000;
+    const int NUM_ITEMS = 500000;
 
     vector<int> a(NUM_ITEMS);
     for (int i = 1; i < a.size(); ++i)
@@ -40,14 +40,11 @@ int main()
     }
 
     srand(time(0) + 1);
-
     permute(a);
 
     auto start = high_resolution_clock::now();
-    // quickSort(a);
     sort(a.begin(), a.end());
     auto stop = high_resolution_clock::now();
-
     checkSort(a);
     auto duration = duration_cast<milliseconds>(stop - start);
     cout << "Sorting took " << duration.count() << " milliseconds." << endl;
