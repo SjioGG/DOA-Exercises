8cb1e3219920ebce26c68c0e111bb3a190256c48
emil874g <113363415+emil874g@users.noreply.github.com>
2023-10-09 23:00:38 +0200

    weird stlsort code

--- Week_5_6_RecursionAndArraySorting/Exercise_7/stlsort.cpp
+++ Week_5_6_RecursionAndArraySorting/Exercise_7/stlsort.cpp
@@ -1,27 +1,55 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <time.h>
+#include <chrono>
+#include <sort.h>
 
 using namespace std;
-template <class T>
-void show(vector<T> v)
+using namespace std::chrono;
+
+void checkSort(const vector<int> &a)
 {
-    cout << "[";
-    for (T i : v)
-        cout << v[i] << ", ";
-    cout << "]";
+    for (int i = 0; i < a.size() - 1; ++i)
+    {
+        if (a[i] > a[i + 1])
+        {
+            cout << "Error at " << i << endl;
+        }
+    }
+    cout << "Finished checksort" << endl;
+}
+
+template <typename AnyType>
+void permute(vector<AnyType> &a)
+{
+    for (int j = 1; j < a.size(); ++j)
+    {
+        swap(a[j], a[rand() % j]);
+    }
 }
 
 int main()
 {
-    vector<int> a = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0};
-    cout << "The vector before sorting is: \n";
-    show(a);
+    const int NUM_ITEMS = 5000000;
 
+    vector<int> a(NUM_ITEMS);
+    for (int i = 1; i < a.size(); ++i)
+    {
+        a[i] = a[i - 1] + 'a';
+    }
+
+    srand(time(0) + 1);
+
+    permute(a);
+
+    auto start = high_resolution_clock::now();
+    // quickSort(a);
     sort(a.begin(), a.end());
+    auto stop = high_resolution_clock::now();
 
-    cout << "\n \n The vector after sorting is: \n";
-    show(a);
-
+    checkSort(a);
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Sorting took " << duration.count() << " milliseconds." << endl;
     return 0;
 }
