d32ec83259ca570e677e470696e50549a54c3c23
emil874g <emilbdl@gmail.com>
2023-10-30 17:25:59 +0100

    something

---
+++ Week_7-8_HeapsAndBinarySearchTrees/Exercise_2/PriorityQueue.cpp
@@ -0,0 +1,26 @@
+template <typename Comparable>
+class PriorityQueue
+{
+public:
+    BinaryHeap<Comparable> bh;
+
+    void pop()
+    {
+        }
+
+    void push(const Comparable &x)
+    {
+    }
+
+    const Comparable &top() const
+    {
+    }
+
+    bool empty()
+    {
+    }
+
+    void clear()
+    {
+    }
+};
